45ec047d87eff359cc056e05fb67cfdbc13beae5
Mehul Kar <mehul.kar@vercel.com>
2023-11-02 22:22:42 -0500

    init git

--- run.py
+++ run.py
@@ -5,4 +5,7 @@ import tempfile
 tmpdir = os.environ["PRYSK_TEMP"] = tempfile.mkdtemp("", "prysk-tests-")
 print("Using temporary directory: %s" % tmpdir)
 
+# initialize a git repoository in the temporary directory
+os.system("git init %s" % tmpdir)
+
 shutil.rmtree(tmpdir)
