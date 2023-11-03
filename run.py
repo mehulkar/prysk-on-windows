ea7939bfbe11de96ad1216784ba1886e94b2b585
Mehul Kar <mehul.kar@vercel.com>
2023-11-02 23:46:05 -0500

    see if we can delete without a git dir

--- run.py
+++ run.py
@@ -2,22 +2,23 @@ import os
 import shutil
 import tempfile
 
-print("tempdir is %s" % tempfile.gettempdir())
+# print("tempdir is %s" % tempfile.gettempdir())
+# os.environ["TMPDIR"] = tempfile.gettempdir()
 
-tmpdir = tempfile.mkdtemp("", "prysk-tests-", "\\tmp")
+tmpdir = tempfile.mkdtemp("", "prysk-tests-")
 print("Using temporary directory: %s" % tmpdir)
 
 # initialize a git repoository in the temporary directory
-os.system("git init $dd --quiet --initial-branch=main" % tmpdir)
+# os.system("git init $dd --quiet --initial-branch=main" % tmpdir)
 os.system("pushd %s && touch foo && popd" % tmpdir)
 os.system("ls -al %s" % tmpdir)
-os.system("ls -al %s/.git" % tmpdir)
+# os.system("ls -al %s/.git" % tmpdir)
 
-os.system("git --git-dir=\"%s/.git\" --work-tree=\"%s\" config user.email \"turbo-test@example.com\"" % (tmpdir, tmpdir))
-os.system("git --git-dir=\"%s/.git\" --work-tree=\"%s\" config user.name \"Turbo Test\"" % (tmpdir, tmpdir))
+# os.system("git --git-dir=\"%s/.git\" --work-tree=\"%s\" config user.email \"turbo-test@example.com\"" % (tmpdir, tmpdir))
+# os.system("git --git-dir=\"%s/.git\" --work-tree=\"%s\" config user.name \"Turbo Test\"" % (tmpdir, tmpdir))
 
-os.system("git --git-dir=\"%s/.git\" --work-tree=\"%s\" add ." % (tmpdir, tmpdir))
-os.system("git --git-dir=\"%s/.git\" --work-tree=\"%s\" commit -m \"Initial\" --quiet" % (tmpdir, tmpdir))
+# os.system("git --git-dir=\"%s/.git\" --work-tree=\"%s\" add ." % (tmpdir, tmpdir))
+# os.system("git --git-dir=\"%s/.git\" --work-tree=\"%s\" commit -m \"Initial\" --quiet" % (tmpdir, tmpdir))
 
 
 print("Deleting %s", tmpdir)
