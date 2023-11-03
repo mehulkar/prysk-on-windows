f286f67d99742740a13742938bfd40aad7bd499c
Mehul Kar <mehul.kar@vercel.com>
2023-11-02 22:52:36 -0500

    Fix syntax error

--- tests/setup.sh
+++ tests/setup.sh
@@ -6,7 +6,11 @@ TARGET_DIR=$1
 THIS_DIR=$(dirname "${BASH_SOURCE[0]}")
 cp -a "${THIS_DIR}/fixture/." "${TARGET_DIR}/"
 git init "${TARGET_DIR}" --quiet --initial-branch=main
-if [[ "$OSTYPE" == "msys" ]]; then $ git config --local core.autocrlf true; fi
+
+if [[ "$OSTYPE" == "msys" ]]; then
+    git config --local core.autocrlf true;
+fi
+
 git --git-dir="${TARGET_DIR}/.git" --work-tree="${TARGET_DIR}" config user.email "turbo-test@example.com"
 git --git-dir="${TARGET_DIR}/.git" --work-tree="${TARGET_DIR}" config user.name "Turbo Test"
 echo "script-shell=$(which bash)" > ${TARGET_DIR}/.npmrc
