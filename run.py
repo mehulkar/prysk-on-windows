8ff43c0004198e926a0fb49e99362c1a46307fff
Mehul Kar <mehul.kar@vercel.com>
2023-11-02 23:53:00 -0500

    -p doesnt work i guess

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ print("Using temporary directory: %s" % tmpdir)
 
 # initialize a git repoository in the temporary directory
 # os.system("git init $dd --quiet --initial-branch=main" % tmpdir)
-os.system("pushd %s && mkdir -p foo && touch foo/bar && popd" % tmpdir)
+os.system("pushd %s && mkdir foo && touch foo/bar.txt && popd" % tmpdir)
 os.system("ls -al %s" % tmpdir)
 # os.system("ls -al %s/.git" % tmpdir)
 
