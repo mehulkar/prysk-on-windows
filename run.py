0230d2e79369bfec8f02253ab357b81eaa304555
Mehul Kar <mehul.kar@vercel.com>
2023-11-02 23:11:58 -0500

    debug

--- run.py
+++ run.py
@@ -8,6 +8,8 @@ print("Using temporary directory: %s" % tmpdir)
 # initialize a git repoository in the temporary directory
 os.system("git init %s --quiet --initial-branch=main" % tmpdir)
 os.system("pushd %s && touch foo && popd" % tmpdir)
+os.system("ls -al %s" % tmpdir)
+os.system("ls -al %s/.git" % tmpdir)
 
 os.system("git --git-dir=\"%s/.git\" --work-tree=\"%s\" config user.email \"turbo-test@example.com\"" % (tmpdir, tmpdir))
 os.system("git --git-dir=\"%s/.git\" --work-tree=\"%s\" config user.name \"Turbo Test\"" % (tmpdir, tmpdir))
