8c6d945d57e4d666c0952c2260aeac8a0d924b6d
Mehul Kar <mehul.kar@vercel.com>
2023-11-02 23:56:05 -0500

    Do it twice

--- run.py
+++ run.py
@@ -2,24 +2,29 @@ import os
 import shutil
 import tempfile
 
-# print("tempdir is %s" % tempfile.gettempdir())
-# os.environ["TMPDIR"] = tempfile.gettempdir()
+print("tempdir is %s" % tempfile.gettempdir())
 
-tmpdir = tempfile.mkdtemp("", "prysk-tests-")
-print("Using temporary directory: %s" % tmpdir)
+############
+tmpdir1 = tempfile.mkdtemp("", "prysk-tests-")
+print("tmpdir1: %s" % tmpdir1)
+os.system("git init $dd --quiet --initial-branch=main" % tmpdir1)
+os.system("pushd %s && mkdir foo && touch foo/bar.txt && popd" % tmpdir1)
+os.system("ls -al %s" % tmpdir1)
+os.system("ls -al %s/.git" % tmpdir1)
+os.system("git --git-dir=\"%s/.git\" --work-tree=\"%s\" config user.email \"turbo-test@example.com\"" % (tmpdir1, tmpdir1))
+os.system("git --git-dir=\"%s/.git\" --work-tree=\"%s\" config user.name \"Turbo Test\"" % (tmpdir1, tmpdir1))
+os.system("git --git-dir=\"%s/.git\" --work-tree=\"%s\" add ." % (tmpdir1, tmpdir1))
+os.system("git --git-dir=\"%s/.git\" --work-tree=\"%s\" commit -m \"Initial\" --quiet" % (tmpdir1, tmpdir1))
 
-# initialize a git repoository in the temporary directory
-# os.system("git init $dd --quiet --initial-branch=main" % tmpdir)
-os.system("pushd %s && mkdir foo && touch foo/bar.txt && popd" % tmpdir)
-os.system("ls -al %s" % tmpdir)
-# os.system("ls -al %s/.git" % tmpdir)
+############
+tmpdir2 = tempfile.mkdtemp("", "prysk-tests-")
+print("tmpdir2: %s" % tmpdir2)
+os.system("pushd %s && mkdir foo && touch foo/bar.txt && popd" % tmpdir2)
+os.system("ls -al %s/foo" % tmpdir2)
 
-# os.system("git --git-dir=\"%s/.git\" --work-tree=\"%s\" config user.email \"turbo-test@example.com\"" % (tmpdir, tmpdir))
-# os.system("git --git-dir=\"%s/.git\" --work-tree=\"%s\" config user.name \"Turbo Test\"" % (tmpdir, tmpdir))
+############
 
-# os.system("git --git-dir=\"%s/.git\" --work-tree=\"%s\" add ." % (tmpdir, tmpdir))
-# os.system("git --git-dir=\"%s/.git\" --work-tree=\"%s\" commit -m \"Initial\" --quiet" % (tmpdir, tmpdir))
-
-
-print("Deleting %s", tmpdir)
-shutil.rmtree(tmpdir)
+print("Deleting %s", tmpdir1)
+shutil.rmtree(tmpdir1)
+print("Deleting %s", tmpdir1)
+shutil.rmtree(tmpdir2)
